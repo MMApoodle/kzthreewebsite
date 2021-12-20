ff2dcfd8df9be589de3ffab5e5337c8e55e3be14
Kzmaster <kzanders@mcad.edu>
2021-12-19 23:19:45 -0600

    first commit

---
+++ kzthree/kzthree/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *  # noqa
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'nz6w)-b03xkow4yc4ddfyr03cy_gm^^bmvj%mvk46i=m3c7-!g'
+
+ALLOWED_HOSTS = ['*']
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+WAGTAIL_CACHE = False
+
+try:
+    from .local_settings import *  # noqa
+except ImportError:
+    pass
